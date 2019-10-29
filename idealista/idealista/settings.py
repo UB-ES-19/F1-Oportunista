8026a17ce21c054a18d77f88ff740b47a1085b14
aadevafi7 <aadevafi7@alumnes.ub.edu>
2019-10-29 18:21:41 +0100

    Define oportunistaDB

--- idealista/idealista/settings.py
+++ idealista/idealista/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'idealista.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'oportunistadb',
+        'NAME': 'oportunistaDB',
         'USER': 'oportunista',
         'PASSWORD': '0p0rtunistA',
         'HOST': 'localhost',
